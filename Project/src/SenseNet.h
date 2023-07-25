fe193384af90aecbf98f3f0d18842f8f6f0e71a9
Mario Cela <mario.cela@outlook.it>
2023-07-25 11:19:30 +0200

    Added SensNet.h with msg struct implementation

---
+++ Project/src/SenseNet.h
@@ -0,0 +1,17 @@
+#ifndef SENSE_NET_H
+#define SENSE_NET_H
+
+typedef nx_struct msg {
+	nx_uint8_t type;
+  nx_uint16_t msg_id;
+  nx_uint16_t data;
+  nx_uint16_t sender;
+  nx_uint16_t destination;
+  nx_uint16_t gateway;
+} msg_t;
+
+enum {
+  AM_RADIO_COUNT_MSG = 10,
+};
+
+#endif
